d5c92bd4d93e53a355817273491132333a05c9d3
Timothy Sikes <tsikes64@gmail.com>
2014-04-04 09:53:56 -0500

    End of class on Friday commit

---
+++ TicTacToe/MaxMin.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TicTacToe
+{
+    public static class MaxMin
+    {
+        public static void orderChildren(bool iAmX, List<Node> children)
+        {
+            children = children.OrderBy(x => x.getHeuristic()).ToList();
+        }
+    }
+}
---
+++ TicTacToe/Node.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
+namespace TicTacToe
+{
+    public class Node
+    {
+        private char[][] board; // uses 'x' and 'o' chars to represent the board.
+        public int level; // refers to the depth level
+        public List<Node> children;
+        public bool min; // Represents whether or not the node is for min or max.
+
+        public Node(char[][] currentBoard, int currentLevel)
+        {
+            // Instantiate a node
+            board = currentBoard;
+            level = currentLevel;
+            generateChildren();
+        }
+
+        public void generateChildren()
+        {
+            // generates all the possible children of this node.
+        }
+
+        public int getHeuristic()
+        {
+            return 0;
+        }
+ 
+    }
+}
