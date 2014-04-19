49821fee24fa60eb7f4f584d223bd095aee81da3
Tyler Wilson <tyler.wilson@eagles.oc.edu>
2014-04-18 19:47:25 -0500

    I feel like I've got it so that it should correctly set Alphas/Betas. Haven't tested it at all yet. Waiting for you Tim. <3

--- TicTacToe/Node.cs
+++ TicTacToe/Node.cs
@@ -10,15 +10,48 @@ namespace TicTacToe
     {
         private char[,] board; // uses 'x' and 'o' chars to represent the board.
         public int level; // refers to the depth level
+        public Node parent;
         public List<Node> children;
         public bool min; // Represents whether or not the node is for min or max.
+        public int alphaBeta;
+        public delegate bool del(int otherAlphaBeta);
+        del better;
 
         public Node(char[,] currentBoard, int currentLevel)
         {
             // Instantiate a node
             board = currentBoard;
             level = currentLevel;
+            //I assume player  controlling x is trying to maximize
+            if(xToMove()) better = x => x > alphaBeta;
             generateChildren();
+        }
+
+        //Delves down to a depth of "depth" to set aplha/beta values
+        public void setAlphaBetas(int depth)
+        {
+            foreach(Node c in children)
+            {
+                //Once the current node becomes an unfeasible choice for its parent then we no longer
+                //need to do update alpha/betas. This is the alpha beta pruning bit
+                if( !parent.better(alphaBeta) ) return;
+
+                //If this is the last layer to do stuff with then we should
+                //set the alpha/beta to the heuristic value
+                if(depth == 0) updateAlphaBeta(getHeuristic());
+                //otherwise recursively call setAlphaBetas on children with a decreased value of depth
+                else setAlphaBetas(depth - 1);
+            }
+            
+        }
+
+        //updates nodes alpha/beta and recursively updates parent as necessary.
+        private void updateAlphaBeta(int val)
+        {
+            alphaBeta = val;
+
+            //Recursively update ancestors
+            if(parent.better(alphaBeta)) updateAlphaBeta(alphaBeta);
         }
 
         private void generateChildren()
@@ -33,6 +66,7 @@ namespace TicTacToe
                         Node newChild = CreateCopy();
                         newChild.level -= 1;
                         newChild.board[i, j] = newChild.xToMove() ? 'x' : 'o';
+                        newChild.parent = this;
                         children.Add(newChild);
                     }
                 }
